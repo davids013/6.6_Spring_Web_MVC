b154b150b4f96dfa2f7d0b692187fb316427f990
Vitaly Shvedko <davids013013@gmail.com>
2022-01-03 17:50:46 +0300

    Migration task completed

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,18 +1,12 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/posts")
 public class PostController {
     public static final String APPLICATION_JSON = "application/json";
@@ -23,35 +17,18 @@ public class PostController {
     }
 
     @GetMapping
-    public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final List<Post> data = service.all();
-        final Gson gson = new Gson();
-        response.getWriter().print(gson.toJson(data));
+    public List<Post> all() {
+        return service.all();
     }
 
     @GetMapping("/{id}")
-    public void getById(@PathVariable long id, HttpServletResponse response) throws IOException {
-        final Gson gson = new Gson();
-        try {
-            final Post data = service.getById(id);
-            response.setContentType(APPLICATION_JSON);
-            response.getWriter().print(gson.toJson(data));
-        } catch (NotFoundException e) {
-            final int status = HttpServletResponse.SC_NOT_FOUND;
-            response.setStatus(status);
-            response.setContentType("text/plain");
-            response.getWriter().print("ERROR " + status + "\nPost " + id + " not found");
-        }
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
     @PostMapping
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final Gson gson = new Gson();
-        final Post post = gson.fromJson(body, Post.class);
-        final Post data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
     @DeleteMapping("/{id}")
