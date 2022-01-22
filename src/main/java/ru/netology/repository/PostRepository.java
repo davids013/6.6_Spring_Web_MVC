513e8fecea8bcb28512b666d5a4f907cb99347fc
Vitaly Shvedko <71638856+davids013@users.noreply.github.com>
2022-01-22 21:25:56 +0300

    Feature/permanent ser post (#2)
    * Removed flag in Post
    
    * Convert Post:isRemoved to transient field
    
    * Return facade of Post

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -3,6 +3,7 @@ package ru.netology.repository;
 import org.springframework.stereotype.Repository;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
+import ru.netology.model.VisiblePost;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 // Stub
 @Repository
@@ -17,33 +19,43 @@ public class PostRepository {
     private final Map<Long, Post> posts = new ConcurrentHashMap<>();
     private final AtomicLong counter = new AtomicLong(0);
 
-    public List<Post> all() {
+    public List<VisiblePost> all() {
         if (posts.isEmpty())
             return new ArrayList<>();
-        return new ArrayList<>(posts.values());
+        return posts
+                .values()
+                .stream()
+                .filter(x -> !x.getRemoved())
+                .map(VisiblePost::new)
+                .collect(Collectors.toList());
     }
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(posts.get(id));
+    public Optional<VisiblePost> getById(long id) {
+        final Post post = posts.get(id);
+        if (post != null && !post.getRemoved()) return Optional.of(new VisiblePost(post));
+        throw new NotFoundException("Can't read. There is no post with id " + id);
     }
 
-    public Post save(Post post) {
+    public VisiblePost save(Post post) {
         final long id = post.getId();
         if (id == 0) {
             final long nextId = counter.incrementAndGet();
             final Post newPost = new Post(nextId, post.getContent());
             posts.put(nextId, newPost);
-            return newPost;
+            return new VisiblePost(newPost);
         } else {
-            if (posts.containsKey(id)) {
+            if (posts.containsKey(id) && !posts.get(id).getRemoved()) {
                 posts.put(id, post);
-                return post;
+                return new VisiblePost(post);
             }
         }
         throw new NotFoundException("Can't override. There is no post with id " + id);
     }
 
     public void removeById(long id) {
-        posts.remove(id);
+        final Post post = posts.get(id);
+        if (post != null && !post.getRemoved()) {
+            post.setRemoved(true);
+        } else throw new NotFoundException("Can't remove. There is no post with id " + id);
     }
 }
